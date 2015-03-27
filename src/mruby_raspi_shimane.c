3c58bbfb2af1209b1cd37fc94c4f6139c3d76c96
raspi <pi@raspberrypi.(none)>
2015-03-27 08:13:40 +0000

    Add Thermal sensor, Raspi.sensor(1) or Raspi.sensor(2)

--- src/mruby_raspi_shimane.c
+++ src/mruby_raspi_shimane.c
@@ -57,11 +57,52 @@ f_gpio_dread(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(value);
 }
 
+
+static mrb_value
+f_sleep(mrb_state *mrb, mrb_value self)
+{
+  char buf[100];
+  mrb_int sec;
+  mrb_get_args(mrb, "i", &sec);
+
+  sprintf(buf, "sleep %d", sec);
+  system(buf);
+
+  return mrb_nil_value();
+}
+
+static mrb_value
+f_sensor(mrb_state *mrb, mrb_value self)
+{
+  char buf[100], chval[100];
+  FILE *fval;
+  mrb_int sensor;
+  double s0, s1, s2, value;
+  mrb_get_args(mrb, "i", &sensor);
+
+  sprintf(buf, "sudo python temp.py");
+  fval=popen(buf, "r");
+  fgets(chval,sizeof(chval),fval);
+  pclose(fval);
+
+  sscanf(chval, "%lf%lf%lf", &s0, &s1, &s2);
+
+  if( sensor == 1 ){
+    value = s1;
+  } else {
+    value = s2;
+  }
+
+  return mrb_fixnum_value(value);
+}
+
+
+
 void
 mrb_mruby_raspi_shimane_gem_init(mrb_state* mrb)
 {
   struct RClass *c;
-  c = mrb_define_module(mrb, "Gpio");
+  c = mrb_define_module(mrb, "Raspi");
 
   mrb_define_const(mrb, c, "OUTPUT", mrb_fixnum_value(1));
   mrb_define_const(mrb, c, "INPUT", mrb_fixnum_value(0));
@@ -71,6 +112,10 @@ mrb_mruby_raspi_shimane_gem_init(mrb_state* mrb)
   mrb_define_module_function(mrb, c, "pinMode", f_gpio_pinmode, MRB_ARGS_REQ(2));
   mrb_define_module_function(mrb, c, "digitalWrite", f_gpio_dwrite, MRB_ARGS_REQ(2));
   mrb_define_module_function(mrb, c, "digitalRead", f_gpio_dread, MRB_ARGS_REQ(1));
+
+  mrb_define_module_function(mrb, c, "sleep", f_sleep, MRB_ARGS_REQ(1));
+
+  mrb_define_module_function(mrb, c, "sensor", f_sensor, MRB_ARGS_REQ(1));
 }
 
 void
